dcef5a7ef47adc5c32e92c28b5ff6e134eb45d79
ankitamaliwal <135990066+ankitamaliwal@users.noreply.github.com>
2025-09-09 10:05:43 +0530

    Create TestGit2.java
    To work with GIT Commands

---
+++ src/main/java/com/qa/opencart/pages/TestGit2.java
@@ -0,0 +1,17 @@
+//New file to test git commands
+package com.qa.opencart.pages;
+
+public class TestingPage {
+	
+	
+
+	int i = 10;
+	String name = "Tom";
+
+	public void test() {
+		System.out.println("test method");
+	}
+	
+	
+
+}
